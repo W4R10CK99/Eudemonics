72f2c69227f9e004768ef3ea049a13062af597f3
ram kumar <rumk78864@gmail.com>
2024-02-07 13:20:40 +0530

    Confidence Score

--- ImageProcessing/finalp2.py
+++ ImageProcessing/finalp2.py
@@ -6,21 +6,25 @@ import json
 # Replace 'your_api_key_here' with your actual OpenAI API key
 myconfig = r"--psm 6 --oem 3"
 
-text = pytesseract.image_to_string(PIL.Image.open('ImageProcessing\check3\Img.JPEG'), config=myconfig)
-
+text= pytesseract.image_to_data(PIL.Image.open('Adobe Scan 19-Jan-2024_2.jpg'),output_type='data.frame', config=myconfig)
+text = text[text.conf != -1]
+conf = text.groupby(['block_num'])['conf'].mean()
+lines = text.groupby('block_num')['text'].apply(list)
 # Initialize OpenAI client with the API key
-client = OpenAI()
+# client = OpenAI()
 
-user_message = {
-    "role": "user",
-    "content": "from the text find attributes and their values in json format " + "\"" + text + " \"",
-}
+# user_message = {
+#     "role": "user",
+#     "content": "from the text find attributes and their values in json format " + "\"" + text + " \"",
+# }
 
-response = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[user_message],
-    max_tokens=1000
-)
+# response = client.chat.completions.create(
+#     model="gpt-3.5-turbo",
+#     messages=[user_message],
+#     max_tokens=1000
+# )
 
-text = response.choices[0].message.content
-print(text)
+# text = response.choices[0].message.content
+# print(text)
+print(conf)
+print(lines)
